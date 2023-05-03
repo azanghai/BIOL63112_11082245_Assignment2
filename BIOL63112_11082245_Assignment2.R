f859b5e0b7602270463bb429965e737f4005accc
azanghai <x5058226@qq.com>
2023-05-03 19:33:01 +0100

    update

--- BIOL63112_11082245_Assignment2.R
+++ BIOL63112_11082245_Assignment2.R
@@ -15,6 +15,14 @@ library(lmerTest)
 # load performance package for model selection
 library(performance)
 
+library(sf)
+
+library(rnaturalearth) # 获取自然地球数据
+library(rnaturalearthdata) # 获取自然地球数据
+library(maps)
+library(olsrr)
+library(modelr)
+
 # variable explanation
 # https://www.kaggle.com/datasets/lashagoch/life-expectancy-who-updated
 # This dataset was corrected and updated to ensure the accurity of the data.
@@ -49,6 +57,10 @@ library(performance)
 # 人口稠密的国家是否往往具有较低的预期寿命？
 # 免疫覆盖率对预期寿命有何影响？
 
+# RQ1: What are the factors that affect life expectancy?
+# RQ2: What is the impact of education on life expectancy?
+# RQ3: 欧盟地区和亚洲地区关于饮酒与预期寿命的关系是什么？
+# RQ4: 预期寿命与GDP的关系如何？
 
 raw_data = read_csv("Life-Expectancy-Data-Updated.csv")
 
@@ -57,6 +69,26 @@ vis_dat(raw_data)
 
 skim(raw_data) %>%
   filter(skim_type == "numeric")
+
+# correct country names for plotting
+raw_data$Country[raw_data$Country == "Turkiye"] = "Turkey"
+raw_data$Country[raw_data$Country == "Russian Federation"] = "Russia"
+raw_data$Country[raw_data$Country == "Bahamas, The"] = "Bahamas"
+raw_data$Country[raw_data$Country =="Gambia, The"] = "Gambia"
+raw_data$Country[raw_data$Country =="Kyrgyz Republic"] = "Kyrgyzstan"
+raw_data$Country[raw_data$Country == "Brunei Darussalam"] = "Brunei"
+raw_data$Country[raw_data$Country == "Yemen, Rep."] = "Yemen"
+raw_data$Country[raw_data$Country =="Micronesia, Fed. Sts."] = "Micronesia"
+raw_data$Country[raw_data$Country =="Congo, Rep."] = "Congo"
+raw_data$Country[raw_data$Country =="Congo, Dem. Rep."] = "Dem. Rep. Congo"
+raw_data$Country[raw_data$Country =="Eswatini"] = "Swaziland"
+raw_data$Country[raw_data$Country =="Egypt, Arab Rep."] = "Egypt"
+raw_data$Country[raw_data$Country =="Syrian Arab Republic"] = "Syria"
+raw_data$Country[raw_data$Country =="Cote d'Ivoire"] = "Ivory Coast"
+raw_data$Country[raw_data$Country =="Iran, Islamic Rep."] = "Iran"
+raw_data$Country[raw_data$Country =="Lao PDR"] = "Laos"
+raw_data$Country[raw_data$Country =="Venezuela, RB"] = "Venezuela"
+raw_data$Country[raw_data$Country =="Central African Republic"] = "Central African Rep."
 
 # merge Economy_status_Developed and Economy_status_Developing into one column
 raw_data = raw_data %>%
@@ -68,7 +100,17 @@ raw_data = raw_data %>%
   mutate(Year = factor(Year)) %>%
   mutate(Economy_status = fct_relevel(Economy_status, "Developed","Developing"))
 
-correlation_data = lapply(raw_data, as.numeric) %>%
+str(raw_data)
+# summarize the data
+summarized_data = raw_data %>%
+  group_by(Country) %>%
+  # summarize mean but keep region and economy status
+  summarize_if(is.numeric, mean) %>%
+  left_join(raw_data %>% select(Country,Region,Economy_status) %>% distinct(), by = "Country")
+
+
+
+correlation_data = lapply(summarized_data, as.numeric) %>%
   as.data.frame() %>%
   as.matrix() %>%
   rcorr()
@@ -128,34 +170,13 @@ corrplot(
 # add the title to the plot
 title("Correration Matrix for All Variables")
 
-# draw a radar plot groupded by Region
-radar_data = raw_data %>%
-  # summarize by region
-  group_by(Region) %>%
-  select_if(is.numeric) %>%
-  summarize_all(mean)
 
 
 
 
-m1 = lm(Life_expectancy ~ Year, data = raw_data)
-summary(m1)
-ggplot(raw_data, aes(x = Year, y = Life_expectancy,colour =Economy_status )) +
-  geom_point() +
-  geom_line(aes(group = Country), size = 1) +
-  labs(title = "Life Expectancy vs Year", x = "Year", y = "Life Expectancy")
 
-raw_data %>%
-  group_by(Country) %>%
-  skim() %>%
-  filter(skim_variable == "Life_expectancy")
 
-names(raw_data)
 
-# analysis the data use Linear mixed model.
-m2 = lmer(Life_expectancy ~ Infant_deaths + Under_five_deaths + Adult_mortality + Alcohol_consumption + Hepatitis_B + Measles + BMI + Polio + Diphtheria + Incidents_HIV + GDP_per_capita + Population_mln + Thinness_ten_nineteen_years + Thinness_five_nine_years + Schooling+(1|Region)+(1|Year), data = raw_data)
-summary(m2)
-check_model(m2)
 
 # 地区热图，综合热图 Y
 # 分地区探索/国家/年份探索————————意义是什么？
@@ -163,3 +184,127 @@ check_model(m2)
 # 如何解释结果
 # 学校在不同地区的生存预测强度如何？
 
+
+
+world <- ne_countries(scale = "medium", returnclass = "sf")
+
+# 将生命周期数据与地图数据合并
+world_data <- merge(world, summarized_data, by.x = "name", by.y = "Country")
+
+
+# 使用ggplot2绘制Choropleth图
+# 其中颜色填充基于Life_expectancy变量（预期寿命）
+ggplot(data = world_data) +
+  geom_sf(aes(fill = Life_expectancy)) + # 基于预期寿命填充颜色
+  scale_fill_gradient(low = "blue", high = "yellow", name = "Life Expectancy") + # 定义颜色渐变
+  theme_minimal() + # 使用简约主题
+  labs(title = "Life Expectancy by Country") # 设置图表标题
+
+# RQ1: What are the factors that affect the life expectancy?
+RQ1.fit1 = lm(Life_expectancy ~ . - Region - Economy_status -Country, data = summarized_data)
+check_model(RQ1.fit1)
+summary(RQ1.fit1)
+
+summarized_data = summarized_data %>%
+  mutate(average_vaccination_rate = rowMeans(select(.,Measles,Hepatitis_B,Polio,Diphtheria))) %>%
+  mutate(average_adcolescent_thinness_rate = rowMeans(select(.,Thinness_ten_nineteen_years,Thinness_five_nine_years)))
+names(summarized_data)
+
+RQ1.fit2 = lm(Life_expectancy ~ . - Region - Economy_status -Country - Infant_deaths - Measles - Hepatitis_B - Polio - Diphtheria - Thinness_ten_nineteen_years - Thinness_five_nine_years, data = summarized_data)
+summary(RQ1.fit2)
+check_model(RQ1.fit2)
+
+RQ1.fit2.step = ols_step_both_p(RQ1.fit2)
+summary(RQ1.fit2.step$model)
+check_model(RQ1.fit2.step$model)
+
+
+# RQ2:What is the impact of education on life expectancy?
+ggplot(data = raw_data,aes(x = Schooling, y = Life_expectancy, color = Region)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  facet_wrap(~Country)
+
+RQ2.fit1 = lmer(Life_expectancy ~ Schooling+(Schooling|Country), data = raw_data)
+summary(RQ2.fit1)
+
+coef(RQ2.fit1)$Country %>%
+  filter(Schooling < 0)
+
+
+# RQ3: 对于饮酒量高的国家，是否会影响预期寿命？
+alcohol_data1 = raw_data %>%
+  filter(Life_expectancy > 80)
+RQ3.fit1 = lmer(Life_expectancy ~ Alcohol_consumption+(Alcohol_consumption|Country), data = alcohol_data1)
+summary(RQ3.fit1)
+check_model(RQ3.fit1)
+coef(RQ3.fit1)
+
+alcohol_data2 = raw_data %>%
+  # filter(Alcohol_consumption>7.78) %>%
+  filter(Region == "European Union")
+
+RQ3.fit2 = lmer(Life_expectancy ~ Alcohol_consumption+(1|Country), data = alcohol_data2)
+summary(RQ3.fit2)
+check_model(RQ3.fit2)
+coef(RQ3.fit2)
+
+alcohol_data3 = raw_data %>%
+  # filter(Alcohol_consumption>7.78) %>%
+  filter(Region == "Asia")
+RQ3.fit3 = lmer(Life_expectancy ~ Alcohol_consumption+(1|Country), data = alcohol_data3)
+summary(RQ3.fit3)
+check_model(RQ3.fit3)
+coef(RQ3.fit3)
+
+alcohol_data2  = alcohol_data2%>%
+  add_predictions(RQ3.fit2,var = "EUpred")
+alcohol_data3  = alcohol_data3%>%
+    add_predictions(RQ3.fit3,var = "Asiapred")
+
+
+ggplot() +
+  geom_point(aes(x =Alcohol_consumption , y =Life_expectancy,colour = Country),data = alcohol_data2) +
+  geom_line(aes(x = Alcohol_consumption, y = EUpred,group = Country), data = alcohol_data2, color = "red") +
+  geom_point(aes(x =Alcohol_consumption , y =Life_expectancy,colour = Country),data = alcohol_data3) +
+  geom_line(aes(x = Alcohol_consumption, y = Asiapred,group = Country), data = alcohol_data3, color = "blue")+
+  geom_point(aes(x =Alcohol_consumption , y =Life_expectancy,colour = Country),data = alcohol_data5) +
+  geom_line(aes(x = Alcohol_consumption, y = AfricaPred,group = Country), data = alcohol_data5, color = "red")
+  # draw prediction line use the model
+
+alcohol_data4 = raw_data %>%
+  filter((Region == "European Union") | (Region == "Asia"))
+
+RQ3.fit4 = lm(Life_expectancy ~ I(Alcohol_consumption^2), data = alcohol_data4)
+summary(RQ3.fit4)
+RQ3.fit5 = lm(Life_expectancy ~ Alcohol_consumption, data = alcohol_data4)
+summary(RQ3.fit5)
+
+alcohol_data4$pred = predict(RQ3.fit4)
+ggplot(data = alcohol_data4, aes(x = Alcohol_consumption, y = Life_expectancy)) +
+  geom_point() +
+  geom_line(aes(y = pred), color = "red")
+
+compare_performance(RQ3.fit4,RQ3.fit5)
+table(raw_data$Region)
+
+# GDP predict life expectancy
+GDP_data = raw_data
+ggplot(data = raw_data, aes(x = GDP_per_capita, y = Life_expectancy,colour = Economy_status)) +
+  geom_point()
+
+RQ4.fit1 = lm(Life_expectancy ~ I(log(GDP_per_capita)), data = raw_data)
+summary(RQ4.fit1)
+check_model(RQ4.fit1)
+GDP_data = GDP_data %>%
+  add_predictions(RQ4.fit1, var = "pred")
+
+coef(RQ4.fit1)
+
+ggplot(data = GDP_data, aes(x = GDP_per_capita, y = Life_expectancy)) +
+    geom_point() +
+    geom_line(aes(y = pred), color = "red")
+
+ggplot(data = raw_data, aes(x = BMI, y = Life_expectancy,colour = Country)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE)
